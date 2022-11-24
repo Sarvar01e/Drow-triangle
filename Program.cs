51be477688c13a480ffa2f655ff261afd44196df
Sarvar01e <sernazarov185@gmail.com>
2022-11-24 03:37:39 +0300

    debug

--- Program.cs
+++ Program.cs
@@ -4,8 +4,15 @@
     {
         static void Main(string[] args)
         {
+
+            pointofreturn:
             Console.Write("Enter hight");
             int hight = int.Parse(Console.ReadLine());
+            if (hight <= 0)
+            {
+                Console.Write("Your number is smaller then or equal 0, reenter");
+                goto pointofreturn;
+            }
 
             int startpoint = 1;
 
